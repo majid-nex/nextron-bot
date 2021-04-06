11d8b3868270ba294c6d42b9bb94b80e270b687d
majid-nex <77442341+majid-nex@users.noreply.github.com>
2021-04-06 18:44:29 +0530

    Create __main__.py

---
+++ userbot/__main__.py
@@ -0,0 +1,128 @@
+import os
+try:
+  from LEGENDX import id, ID, devs, LEGEND
+except:
+  os.system("pip install LEGENDX")
+  from LEGENDX import id, ID, devs, LEGEND
+else:
+  print ("something got fucked")
+finally:
+  print ("ULTRA X IS STARTING WITH TELETHON")
+  from ULTRAX import xbot
+from ULTRA import bot
+from sys import argv
+import sys
+import os
+os.system("pip install google_trans_new")
+import glob
+from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
+import os
+from telethon import TelegramClient, Button
+from var import Var
+from ULTRA.utils import load_module, load_pro
+from ULTRA import LOAD_PLUG, BOTLOG_CHATID
+from pathlib import Path
+import asyncio
+TOKEN = os.environ.get("TG_BOT_TOKEN", None)
+import telethon.utils
+EXTRA_PLUGS = os.environ.get("EXTRA_PLUGS", False)
+async def add_bot(bot_token):
+    await bot.start(bot_token)
+    bot.me = await bot.get_me() 
+    bot.uid = telethon.utils.get_peer_id(bot.me)
+
+
+
+if len(argv) not in (1, 3, 4):
+    bot.disconnect()
+else:
+    bot.tgbot = None
+    if Var.TG_BOT_USER_NAME_BF_HER is not None:
+        print("Initiating Inline Bot")
+        # ForTheGreatrerGood of beautification
+        bot.tgbot = TelegramClient(
+            "TG_BOT_TOKEN",
+            api_id=Var.APP_ID,
+            api_hash=Var.API_HASH
+        ).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
+        print("Initialisation finished with no errors")
+        print("Starting Userbot")
+        bot.loop.run_until_complete(add_bot(Var.TG_BOT_USER_NAME_BF_HER))
+        print("Startup Completed")
+    else:
+        bot.start()
+
+import glob
+
+
+
+path = 'ULTRA/plugins/assistant/*.py'
+files = glob.glob(path)
+for name in files:
+    with open(name) as f:
+        path1 = Path(f.name)
+        shortname = path1.stem
+        load_pro(shortname.replace(".py", ""))
+
+
+if  EXTRA_PLUGS == True:
+    os.system("git clone https://github.com/ULTRA-OP/ULTRA_PLUGS.git ./ULTRA/plugins/")
+    path = "ULTRA/plugins/*.py"
+    files = glob.glob(path)
+    for name in files:
+        with open(name) as a:
+            patt = Path(a.name)
+            plugin_name = patt.stem
+            try:
+                load_module(plugin_name.replace(".py", ""))
+                if not plugin_name.startswith("__") or plugin_name.startswith("_"):
+                    print ('INSTALLING ALL MODULES', plugin_name)
+            except:
+                pass
+
+else:
+  path = 'ULTRA/plugins/*.py'
+  files = glob.glob(path)
+  for name in files:
+      with open(name) as f:
+          path1 = Path(f.name)
+          shortname = path1.stem
+          load_module(shortname.replace(".py", ""))
+
+
+import ULTRA._core
+
+print("Nextron is Awake! ©️ Teamwasp")
+async def legend():
+  try:
+Assis= await xbot.get_me()
+    Master = await bot.get_me()
+    PROBOYX = [[Button.inline("Hᴇʀᴏᴋᴜ Vᴀʀs", data='ass_back')]]
+    Nextron = f"""
+**Sᴏᴍᴇᴛʜɪɴɢ Hᴀᴘᴘᴇɴᴇᴅ ! Lᴇᴛs Cʜᴇᴄᴋ** 🤔 
+
+`☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎`
+
+**Dɪɴɢ Dᴏɴɢ...** `.\./.\` **Tɪɴɢ Tᴏɴɢ...** `./.\./` **Nextron Hᴀs Bᴇᴇɴ Dᴇᴘʟᴏʏᴇᴅ !!**
+
+**Pɪɴɢ Pᴏɴɢ...**
+
+**➥ Mᴀsᴛᴇʀ** `➪` **@{Master.username}**
+**➥ Assɪsᴛᴀɴᴛ** `➪` **@{Assistant.username}**
+**➥ Sᴜᴘᴘᴏʀᴛ** `➪` **@NextronchaT**
+**➥ Cʜᴀɴɴᴇʟ** `➪` **@NextronSupporT**
+
+**Cʜᴇᴄᴋ ᴍᴏɪ Pɪɴɢ ᴛɪᴍᴇ ʙʏ** `.ping` **[Fᴏʀ UsᴇʀBᴏᴛ] or** `/ping` **[Fᴏʀ Assɪsᴛᴀɴᴛ]**
+"""
+    await xbot.send_message(bot.me.id, NEXTRON, buttons=PROBOYX)
+  except:
+     pass
+
+
+bot.loop.run_until_complete(legend())
+if len(argv) not in (1, 3, 4):
+    bot.disconnect()
+    
+else:
+    bot.run_until_disconnected()
+   
