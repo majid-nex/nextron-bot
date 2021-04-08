3a4f9c3d21a3932b9ffe174d08885f86d4fedb5e
majid-nex <77442341+majid-nex@users.noreply.github.com>
2021-04-08 22:03:17 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,31 +1,16 @@
-import os
-try:
-  from userbot import id, ID, devs, LEGEND
-except:
-  os.system("pip install LEGENDX")
-  from userbot import id, ID, devs, LEGEND
-else:
-  print ("something got fucked")
-finally:
-  print ("NEXTRON IS STARTING WITH TELETHON")
-  from userbot import xbot
-from NEXTRON import bot
+from userbot import bot
 from sys import argv
 import sys
-import os
-os.system("pip install google_trans_new")
-import glob
 from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
 import os
-from telethon import TelegramClient, Button
+from telethon import TelegramClient
 from var import Var
-from userbot.utils import load_module, load_pro
-from NEXTRON import LOAD_PLUG, BOTLOG_CHATID
+from userbot.utils import load_module
+from userbot import LOAD_PLUG, BOTLOG_CHATID, LOGS
 from pathlib import Path
 import asyncio
-TOKEN = os.environ.get("TG_BOT_TOKEN", None)
 import telethon.utils
-EXTRA_PLUGS = os.environ.get("EXTRA_PLUGS", False)
+
 async def add_bot(bot_token):
     await bot.start(bot_token)
     bot.me = await bot.get_me() 
@@ -51,78 +36,22 @@ else:
         print("Startup Completed")
     else:
         bot.start()
+    
 
 import glob
-
-
-
-path = 'plugins/assistant/*.py'
+path = 'plugins/*.py'
 files = glob.glob(path)
 for name in files:
     with open(name) as f:
         path1 = Path(f.name)
         shortname = path1.stem
-        load_pro(shortname.replace(".py", ""))
-
-
-if  EXTRA_PLUGS == True:
-    os.system("git clone https://github.com/majid-nex/NEXTRON_PLUGS.git ./ULTRA/plugins/")
-    path = "plugins/*.py"
-    files = glob.glob(path)
-    for name in files:
-        with open(name) as a:
-            patt = Path(a.name)
-            plugin_name = patt.stem
-            try:
-                load_module(plugin_name.replace(".py", ""))
-                if not plugin_name.startswith("__") or plugin_name.startswith("_"):
-                    print ('INSTALLING ALL MODULES', plugin_name)
-            except:
-                pass
-
-else:
-  path = 'plugins/*.py'
-  files = glob.glob(path)
-  for name in files:
-      with open(name) as f:
-          path1 = Path(f.name)
-          shortname = path1.stem
-          load_module(shortname.replace(".py", ""))
-
+        load_module(shortname.replace(".py", ""))
 
 import userbot._core
 
-print("Nextron is Awake! ©️ Teamwasp")
-async def legend():
-  try:
-Assis= await xbot.get_me()
-    Master = await bot.get_me()
-    PROBOYX = [[Button.inline("Hᴇʀᴏᴋᴜ Vᴀʀs", data='ass_back')]]
-    Nextron = f"""
-**Sᴏᴍᴇᴛʜɪɴɢ Hᴀᴘᴘᴇɴᴇᴅ ! Lᴇᴛs Cʜᴇᴄᴋ** 🤔 
+print("NEXTRON BOT is on fire 🔥🔥🔥 all files installed on your bot.... Join @Nextronsupport for any help..")
 
-`☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎`
-
-**Dɪɴɢ Dᴏɴɢ...** `.\./.\` **Tɪɴɢ Tᴏɴɢ...** `./.\./` **Nextron Hᴀs Bᴇᴇɴ Dᴇᴘʟᴏʏᴇᴅ !!**
-
-**Pɪɴɢ Pᴏɴɢ...**
-
-**➥ Mᴀsᴛᴇʀ** `➪` **@{Master.username}**
-**➥ Assɪsᴛᴀɴᴛ** `➪` **@{Assistant.username}**
-**➥ Sᴜᴘᴘᴏʀᴛ** `➪` **@NextronchaT**
-**➥ Cʜᴀɴɴᴇʟ** `➪` **@NextronSupporT**
-
-**Cʜᴇᴄᴋ ᴍᴏɪ Pɪɴɢ ᴛɪᴍᴇ ʙʏ** `.ping` **[Fᴏʀ UsᴇʀBᴏᴛ] or** `/ping` **[Fᴏʀ Assɪsᴛᴀɴᴛ]**
-"""
-    await xbot.send_message(bot.me.id, NEXTRON, buttons=PROBOYX)
-  except:
-     pass
-
-
-bot.loop.run_until_complete(legend())
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
-    
 else:
     bot.run_until_disconnected()
-   
