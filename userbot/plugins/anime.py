cb7843f5c8e33c348aaa43bab9d4612b1615cf6a
majid-nex <77442341+majid-nex@users.noreply.github.com>
2021-04-10 13:51:04 +0530

    Update anime.py

--- userbot/plugins/anime.py
+++ userbot/plugins/anime.py
@@ -1,32 +1,32 @@
 import re
 
-from hellbot import bot
-from hellbot.utils import admin_cmd, sudo_cmd, edit_or_reply
-from hellbot.cmdhelp import CmdHelp
-from hellbot.helpers.functions import deEmojify
+from userbot import bot
+from userbot.utils import admin_cmd, sudo_cmd, edit_or_reply
+from userbot.cmdhelp import CmdHelp
+from userbot.helpers.functions import deEmojify
 
 
 @bot.on(admin_cmd(pattern="anime(?: |$)(.*)"))
 @bot.on(sudo_cmd(pattern="anime(?: |$)(.*)", allow_sudo=True))
-async def nope(kraken):
-    hell = kraken.pattern_match.group(1)
-    if not hell:
-        if kraken.is_reply:
-            (await kraken.get_reply_message()).message
+async def nope(NEX-GEN):
+    Nextron = NEX-GEN.pattern_match.group(1)
+    if not Nextron:
+        if NEX-GEN.is_reply:
+            (await NEX-GEN.get_reply_message()).message
         else:
-            await edit_or_reply(kraken, "`Sir please give some query to search and download it for you..!`"
+            await edit_or_reply(NEX-GEN, "`Sir please give some query to search and download it for you..!`"
             )
             return
 
-    troll = await bot.inline_query("animedb_bot", f"{(deEmojify(hell))}")
+    troll = await bot.inline_query("animedb_bot", f"{(deEmojify(nex))}")
 
     await troll[0].click(
-        kraken.chat_id,
-        reply_to=kraken.reply_to_msg_id,
-        silent=True if kraken.is_reply else False,
+        NEX-GEN.chat_id,
+        reply_to=NEX-GEN.reply_to_msg_id,
+        silent=True if NEX-GEN.is_reply else False,
         hide_via=True,
     )
-    await kraken.delete()
+    await NEX-GEN.delete()
     
 
 CmdHelp("anime").add_command(
