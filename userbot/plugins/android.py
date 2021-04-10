feb97d40776030d971c155e74e4ee5f72de0ac37
majid-nex <77442341+majid-nex@users.noreply.github.com>
2021-04-10 13:42:53 +0530

    Update android.py

--- userbot/plugins/android.py
+++ userbot/plugins/android.py
@@ -13,7 +13,7 @@ from requests import get
 from userbot.cmdhelp import CmdHelp
 
 from userbot import *
-from hellbot.utils import *
+from userbot.utils import *
 
 GITHUB = "https://github.com"
 DEVICES_DATA = (
