f493ebb43f4e45200c3afa9b2e438c696e0cf650
majid-nex <77442341+majid-nex@users.noreply.github.com>
2021-04-10 14:19:38 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,16 +1,31 @@
+import os
+try:
+  from userbot import id, ID, devs, LEGEND
+except:
+  os.system("pip install LEGENDX")
+  from userbot import id, ID, devs, LEGEND
+else:
+  print ("something got fucked")
+finally:
+  print ("NEXTRON IS STARTING WITH TELETHON")
+  from NEXTRON import xbot
 from userbot import bot
 from sys import argv
 import sys
+import os
+os.system("pip install google_trans_new")
+import glob
 from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
 import os
-from telethon import TelegramClient
+from telethon import TelegramClient, Button
 from var import Var
-from userbot.utils import load_module
-from userbot import LOAD_PLUG, BOTLOG_CHATID, LOGS
+from userbot.utils import load_module, load_pro
+from userbot import LOAD_PLUG, BOTLOG_CHATID
 from pathlib import Path
 import asyncio
+TOKEN = os.environ.get("TG_BOT_TOKEN", None)
 import telethon.utils
-
+EXTRA_PLUGS = os.environ.get("EXTRA_PLUGS", False)
 async def add_bot(bot_token):
     await bot.start(bot_token)
     bot.me = await bot.get_me() 
@@ -36,22 +51,73 @@ else:
         print("Startup Completed")
     else:
         bot.start()
-    
 
 import glob
-path = 'plugins/*.py'
+
+
+
+path = 'ULTRA/plugins/assistant/*.py'
 files = glob.glob(path)
 for name in files:
     with open(name) as f:
         path1 = Path(f.name)
         shortname = path1.stem
-        load_module(shortname.replace(".py", ""))
+        load_pro(shortname.replace(".py", ""))
 
-import userbot._core
 
-print("NEXTRON BOT is on fire 🔥🔥🔥 all files installed on your bot.... Join @Nextronsupport for any help..")
+if  EXTRA_PLUGS == True:
+    os.system("git clone https://github.com/ULTRA-OP/ULTRA_PLUGS.git ./ULTRA/plugins/")
+    path = "ULTRA/plugins/*.py"
+    files = glob.glob(path)
+    for name in files:
+        with open(name) as a:
+            patt = Path(a.name)
+            plugin_name = patt.stem
+            try:
+                load_module(plugin_name.replace(".py", ""))
+                if not plugin_name.startswith("__") or plugin_name.startswith("_"):
+                    print ('INSTALLING ALL MODULES', plugin_name)
+            except:
+                pass
 
+else:
+  path = 'ULTRA/plugins/*.py'
+  files = glob.glob(path)
+  for name in files:
+      with open(name) as f:
+          path1 = Path(f.name)
+          shortname = path1.stem
+          load_module(shortname.replace(".py", ""))
+
+
+import ULTRA._core
+
+print("UltraX is Up and Awake! ©️ TeamUltraX 2021")
+async def legend():
+  try:
+    pro = await xbot.get_me()
+    legend = await bot.get_me()
+    PROBOYX = [[Button.inline("Hᴇʀᴏᴋᴜ Vᴀʀs", data='ass_back')]]
+    LEGENDX = f"""
+**Sᴏᴍᴇᴛʜɪɴɢ Hᴀᴘᴘᴇɴᴇᴅ ! Lᴇᴛs Cʜᴇᴄᴋ** 🤔 
+`☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎`
+**Dɪɴɢ Dᴏɴɢ...** `.\./.\` **Tɪɴɢ Tᴏɴɢ...** `./.\./` **UʟᴛʀᴀX Hᴀs Bᴇᴇɴ Dᴇᴘʟᴏʏᴇᴅ !!**
+**Pɪɴɢ Pᴏɴɢ...**
+**➥ Mᴀsᴛᴇʀ** `➪` **@{legend.username}**
+**➥ Assɪsᴛᴀɴᴛ** `➪` **@{pro.username}**
+**➥ Sᴜᴘᴘᴏʀᴛ** `➪` **@UltraXchaT**
+**➥ Cʜᴀɴɴᴇʟ** `➪` **@UltraX_SupporT**
+**Cʜᴇᴄᴋ ᴍᴏɪ Pɪɴɢ ᴛɪᴍᴇ ʙʏ** `.ping` **[Fᴏʀ UsᴇʀBᴏᴛ] or** `/ping` **[Fᴏʀ Assɪsᴛᴀɴᴛ]**
+"""
+    await xbot.send_message(bot.me.id, LEGENDX, buttons=PROBOYX)
+  except:
+     pass
+
+
+bot.loop.run_until_complete(legend())
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
+    
 else:
     bot.run_until_disconnected()
+    
