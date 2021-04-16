2afd97ffaa3e3df7db60bf13f65158f5e1577b73
majid-nex <77442341+majid-nex@users.noreply.github.com>
2021-04-16 21:26:28 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,60 +1,50 @@
-from userbot import *
-from hellbot.utils import *
-from userbot.cmdhelp import CmdHelp
-from telethon import events, version
-from telethon.events import NewMessage
-from telethon.tl.custom import Dialog
-from telethon.tl.types import Channel, Chat, User
-
-#-------------------------------------------------------------------------------
-
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Hell User"
-
-ludosudo = Config.SUDO_USERS
-
-if ludosudo:
-    sudou = "True"
-else:
-    sudou = "False"
-
-kraken = bot.uid
-
-PM_IMG = "https://telegra.ph/file/80e5200c615cf0cb57aa9.mp4"
-pm_caption = "__**🔥🔥ɦɛʟʟɮօt ɨs օռʟɨռɛ🔥🔥**__\n\n"
-
-pm_caption += (
-    f"               __↼🄼🄰🅂🅃🄴🅁⇀__\n**『 [{DEFAULTUSER}](tg://user?id={kraken}) 』**\n\n"
-)
-
-pm_caption += f"🛡️TELETHON🛡️ : `{version.__version__}` \n"
-
-pm_caption += f"😈Hêllẞø†😈       : __**{hellversion}**__\n"
-
-pm_caption += f"⚜️Sudo⚜️            : `{sudou}`\n"
-
-pm_caption += "⚠️CHANNEL⚠️   : [ᴊᴏɪɴ](https://t.me/HellBot_Official)\n"
-
-pm_caption += "🔥CREATOR🔥    : [Nub Here](https://t.me/SupRemE_AnanD)\n\n"
-
-pm_caption += "    [✨REPO✨](https://github.com/hellboy-op/hellbot) 🔹 [📜License📜](https://github.com/HellBoy-OP/HellBot/blob/master/LICENSE)"
+from telethon import events, Button, custom
+import re, os
+from NEXTRON import PHOTO, xbot, BOT, VERSION
+from userbot import bot
+@xbot.on(events.NewMessage(pattern=("/alive|/start")))
+async def awake(event):
+  NEXTRON = f"ʜᴇʟʟᴏ ᴛʜɪs ɪs  {BOT}\n\n"
+  NEXTRON += "ᴀʟʟ sʏsᴛᴇᴍ ɪs ᴡᴏʀᴋɪɴɢ ᴘʀᴏᴘᴇʀʟʏ\n\n"
+  NEXTRON += f"{BOT} VERSION : {VERSION} ʟᴀsᴛᴇsᴛ\n\n"
+  NEXTRON += f"ᴍʏ ᴍᴀsᴛᴇʀ @{bot.me.username} ☺️\n\n"
+  NEXTRON += "ғᴜʟʟʏ ᴜᴘᴅᴀᴛᴇᴅ ʙᴏᴛ\n\n"
+  NEXTRON += "ᴛᴇʟᴇᴛʜᴏɴ : 1.20 LATEST\n\n"
+  NEXTRON += "ᴛʜᴀɴᴋs ғᴏʀ ᴄʜᴇᴄᴋɪɴɢ ᴍᴇ"
+  BUTTON = [[Button.url("𝙼𝙰𝚂𝚃𝙴𝚁", f"https://t.me/{bot.me.username}"), Button.url(f"{BOT} 𝚁𝙴𝙿𝙾", "https://github.com/LEGENDXOP/LEGEND-BOT")]]
+  BUTTON += [[custom.Button.inline("𝚁𝙴𝙿𝙾𝚂𝙸𝚃𝙾𝚁𝚈", data="NEXTRON")]]
+  await xbot.send_file(event.chat_id, PHOTO, caption=LEGENDX,  buttons=BUTTON)
 
 
-@bot.on(admin_cmd(outgoing=True, pattern="alive$"))
-@bot.on(sudo_cmd(pattern="alive$", allow_sudo=True))
-async def amireallyalive(alive):
-    if alive.fwd_from:
-        return
-    await alive.get_chat()
-    await alive.delete()
-    """ For .alive command, check if the bot is running.  """
-    await borg.send_file(alive.chat_id, PM_IMG, caption=pm_caption)
-    await alive.delete()
 
 
-CmdHelp("alive").add_command(
-  'alive', None, 'Check weather the bot is alive or not'
-).add_command(
-  'hell', None, 'Check weather the bot is alive or not. In your custom Alive Pic and Alive Msg'
-).add_info(
-  'Zinda Hai Kya Bro?'
-).add()
+@xbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"NEXTRON")))
+async def callback_query_handler(event):
+# inline by LEGENDX22 and PROBOY22 🔥
+  PROBOYX = [[Button.url("REPO-LEGEND", "https://github.com/LEGENDXOP/LEGEND-BOT")]]
+  PROBOYX +=[[Button.url("DEPLOY-LEGEND", "https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2FLEGENDXOP%2Flegendpack&template=https%3A%2F%2Fgithub.com%2FLEGENDXOP%2Flegendpack")]]
+  PROBOYX +=[[Button.url("TUTORIAL", "https://youtu.be/rGCSSFPsS4Q"), Button.url("STRING-SESSION", "https://repl.it/@legendx22/LEGEND-BOT#main.py")]]
+  PROBOYX +=[[Button.url("API_ID & HASH", "https://t.me/usetgxbot"), Button.url("REDIS", "https://redislabs.com")]]
+  PROBOYX +=[[Button.url("SUPPORT CHANNEL", "https://t.me/LEGENDBOT_OFFICIAL"), Button.url("SUPPORT GROUP", "https://t.me/LEGEND_USERBOT_SUPPORT")]]
+  PROBOYX +=[[custom.Button.inline("ALIVE", data="PROBOY")]]
+  await event.edit(text=f"𝙰𝙻𝙻 𝙳𝙴𝚃𝙰𝙸𝙻𝚂 𝙾𝙵 𝚁𝙴𝙿𝙾𝚂", buttons=PROBOYX)
+
+
+@xbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"PROBOY")))
+async def callback_query_handler(event):
+# inline by LEGENDX22 and PROBOY22 🔥
+  NEXTRON = f"ʜᴇʟʟᴏ ᴛʜɪs ɪs  {BOT}\n\n"
+  NEXTRON += "ᴀʟʟ sʏsᴛᴇᴍ ɪs ᴡᴏʀᴋɪɴɢ ᴘʀᴏᴘᴇʀʟʏ\n\n"
+  NEXTRON += f"{BOT} OS : {VERSION} ʟᴀsᴛᴇsᴛ\n\n"
+  NEXTRON += f"ᴍʏ ᴍᴀsᴛᴇʀ @{bot.me.username} ☺️\n\n"
+  NEXTRON += "ғᴜʟʟʏ ᴜᴘᴅᴀᴛᴇᴅ ʙᴏᴛ\n\n"
+  NEXTRON += "ᴛᴇʟᴇᴛʜᴏɴ : 1.20 LATEST\n\n"
+  NEXTRON += "ᴛʜᴀɴᴋs ғᴏʀ ᴄʜᴇᴄᴋɪɴɢ ᴍᴇ"
+  BUTTONS = [[Button.url("𝙼𝙰𝚂𝚃𝙴𝚁", f"https://t.me/{bot.me.username}"), Button.url(f"{BOT} 𝚁𝙴𝙿𝙾", "https://github.com/LEGENDXOP/LEGEND-BOT")]]
+  BUTTONS += [[custom.Button.inline("𝚁𝙴𝙿𝙾𝚂𝙸𝚃𝙾𝚁𝚈", data="NEXTRON")]]
+  await event.edit(text=LEGENDX, buttons=BUTTONS)
+
+
+@xbot.on(events.NewMessage(pattern=("/repo|#repo")))
+async def repo(event):
+  await xbot.send_message(event.chat, "ʀᴇᴘᴏ ᴏғ NEXTRON", buttons=[[Button.url("⚜️ ʀᴇᴘᴏ ⚜️", "https://github.com/LEGENDXOP/LEGEND-BOT")]])
