a01b8394b47ae176ee066d40c6c2f3a12c1fc7a0
majid-nex <77442341+majid-nex@users.noreply.github.com>
2021-04-26 22:11:16 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -7,7 +7,7 @@ from telethon import TelegramClient
 from var import Var
 from userbot.Config import Config
 from userbot.utils import load_module
-from userbot import LOAD_PLUG, LOGS, hellversion
+from userbot import LOAD_PLUG, LOGS
 from pathlib import Path
 import asyncio
 import telethon.utils
