3216b4653d6b8adef91561b32bbb4eca48fe6669
majid-nex <77442341+majid-nex@users.noreply.github.com>
2021-04-27 08:24:19 +0530

    Update awakes.py

--- userbot/plugins/awakes.py
+++ userbot/plugins/awakes.py
@@ -19,19 +19,19 @@ from platform import python_version, uname
 
 ALIVE_PIC = Config.ALIVE_PHOTTO
 if ALIVE_PIC is None:
-   ALIVE_PIC = "https://telegra.ph/file/21e8ba90ef8b22fa6a864.jpg"
+   ALIVE_PIC = "https://telegra.ph/file/ada158c12209658274f0d.jpg"
 
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
 
 ALIVE_MESSAGE = Config.ALIVE_MSG
 if ALIVE_MESSAGE is None:
-   ALIVE_MESSAGE = "**🔱LEGEND BOT IS Awake🔱 \n\n\n**"
+   ALIVE_MESSAGE = "**🔱NEXTRON BOT IS Awake🔱 \n\n\n**"
    ALIVE_MESSAGE += "`My Bot Status \n\n\n`"
    ALIVE_MESSAGE += f"`Telethon: TELETHON-15.0.0 \n\n`"
    ALIVE_MESSAGE += f"`Python: PYTHON-3.8.5 \n\n`"
    ALIVE_MESSAGE += "`I'll Be With You Master Till My Dyno Ends!!☠ \n\n`"
-   ALIVE_MESSAGE += f"`Support Channel` : @hackerget0 \n\n"
+   ALIVE_MESSAGE += f"`Support Channel` : @NEXTRONSUPPORT \n\n"
    ALIVE_MESSAGE += f"`MY BOSS🤗`: {DEFAULTUSER} \n\n "
                 
             
