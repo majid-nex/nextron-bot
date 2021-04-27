ef19b64b115fe00fb99a6a8670b948ad0c3251a2
majid-nex <77442341+majid-nex@users.noreply.github.com>
2021-04-27 15:50:53 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -7,7 +7,7 @@ from telethon import events, functions
 from telethon.tl.functions.users import GetFullUserRequest
 
 from userbot.plugins.sql_helper import pmpermit_sql as pmpermit_sql
-from userbot import ALIVE_NAME, CUSTOM_PMPERMIT, hell_ID
+from userbot import ALIVE_NAME, CUSTOM_PMPERMIT
 from userbot.Config import Config
 from hellbot.utils import admin_cmd
 from userbot.cmdhelp import CmdHelp
