fcfa08634f4b6bd90f7e0e90cf4fcd784bee53bd
majid-nex <77442341+majid-nex@users.noreply.github.com>
2021-04-27 14:45:22 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -303,7 +303,7 @@ if Var.PRIVATE_GROUP_ID is not None:
         if ULTRA_X.is_approved(chat_ids):
             return
         if not ULTRA_X.is_approved(chat_ids):
-            await LEGENDX (ultra, "pmsecurity")
+            await LEGENDX (userbot, "pmsecurity")
 
     
 
