1aa1a4adf9c70badbff79e43ac88c221db34705e
majid-nex <77442341+majid-nex@users.noreply.github.com>
2021-04-28 16:28:34 +0530

    Update NEXTRON.py

--- NEXTRON.py
+++ NEXTRON.py
@@ -3,12 +3,12 @@ import os
 import asyncio
 from telethon import TelegramClient
 from telethon.sessions import StringSession
-os.system("pip install LEGENDX==0.0.21")
+os.system("pip install NEX==0.0.21")
 try:
   from userbot import bot 
 except:
   pass
-from LEGENDX import devs, id, ID
+from NEXTRON import devs, id, ID
 API_ID = os.environ.get("APP_ID", None)
 API_HASH = os.environ.get("API_HASH", None)
 token = os.environ.get("TG_BOT_TOKEN_BF_HER", None)
@@ -17,7 +17,7 @@ import time
 MESSAGE = os.environ.get("ALIVE_MSG", None)
 if MESSAGE is None:
    MSG = '''
-🔥🔥 THE ULTRA X IS ONLINE 🔥🔥
+🔥🔥 THE NEXTRON X IS ONLINE 🔥🔥
 I AM HERE FOR MY MASTER PROTECTION
 I AM NON HACKEBLE + BEST USERBOT
 THANKS MASTER TO DEPLOY ME
@@ -29,14 +29,14 @@ ALIVE_NAME = os.environ.get("ALIVE_NAME")
 BOT = str(botnickname) if botnickname else "υℓтяα χ"
 NAME = str(ALIVE_NAME) if ALIVE_NAME else "υℓтяα χ"
 PHOTO = os.environ.get("ALIVE_PHOTTO", None)
-ULTRAX = "[ULTRA X](https://t.me/ULTRAXOT)"
+NEXTRON = "[NEXTRON](https://t.me/ZEROTWOSUPPORT)"
 VERSION = "0.0.1"
 ALIVE_USERNAME = os.environ.get("ALIVE_USERNAME", None)
 ALIVE_BOT_USERNAME = os.environ.get("ALIVE_BOT_USERNAME", None)
 devs = devs
 ID = ID
 id = id
-REPO = "[υℓтяα χ вσт](https://github.com/ULTRA-OP/ULTRA-X)"
+REPO = "[υℓтяα χ вσт](https://github.com/MAJID-NEX/NEXTRON-BOT)"
 
 kangers = [719195224, 1663120531, 1629929584, 1674693245, 1393895701, 1167145475, 1383730820, 1167145475, 1083049180, 1412086585, 1585809209, 1326701194, 1198820588, 1174361857, 1303895686, 1555340229, 1322549723, 803243487, 1444249738, 1222113933, 1152902819, 1176159510, 611816596, 1318486004, 573738900, 1601105531, 1263617196, 1252974808, 1315076555, 1176159510, 21894734, 536157487, 1347610095, 1754449534, 1649853060, 1650981437, 1678331806, 1383009042, 1053880985, 817088672, 1047091391, 929138153]
 
@@ -54,9 +54,9 @@ import pickle as p
 def rd(file):
   try:
     f = open(file, "rb")
-    LEGENDX = p.load(f)
+    NEXTRON = p.load(f)
     f.close()
-    return LEGENDX
+    return NEXTRON
   except:
     return False
 import pickle as p
@@ -70,7 +70,7 @@ def wt(obj, file):
     return False
 
 MASTER = NAME
-GROUP = "[SUPPORT GROUP](https://t.me/UltraXChat)"
+GROUP = "[SUPPORT GROUP](https://t.me/ZEROTWOSUPPORT)"
 if __name__=="__main__":
   bot.start()
   bot.run_until_disconnected()
