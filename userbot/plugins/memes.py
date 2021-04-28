35300c154219d78dc76948826a665069910fbe50
majid-nex <77442341+majid-nex@users.noreply.github.com>
2021-04-28 16:33:22 +0530

    Update memes.py

--- userbot/plugins/memes.py
+++ userbot/plugins/memes.py
@@ -845,10 +845,10 @@ async def vapor(vpr):
 async def repo(event):
     if event.fwd_from:
         return
-    LEGENDX = Var.TG_BOT_USER_NAME_BF_HER
+    NEXTRON = Var.TG_BOT_USER_NAME_BF_HER
     if event.reply_to_msg_id:
         await event.get_reply_message()
-    response = await bot.inline_query(LEGENDX, "repo")
+    response = await bot.inline_query(NEXTRON, "repo")
     await response[0].click(event.chat_id)
     await event.delete()
 
