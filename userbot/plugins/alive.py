3fae8dbedf78c537441da6c8f480aab72bd3b6f6
majid-nex <77442341+majid-nex@users.noreply.github.com>
2021-05-01 13:24:36 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,17 +1,10 @@
-# modify by LEGENDX22
-# Kang with credits else gay..
-import asyncio
-import os
-import requests
-import time
-from PIL import Image
-from io import BytesIO
-from datetime import datetime
-import random
-from telethon import events
-from userbot.utils import admin_cmd, sudo_cmd
-from userbot import ALIVE_NAME
-from telethon.tl.types import ChannelParticipantsAdmins
+from userbot import *
+from userbot.utils
+from userbot.cmdhelp import CmdHelp
+from telethon import events, version
+from telethon.events import NewMessage
+from telethon.tl.custom import Dialog
+from telethon.tl.types import Channel, Chat, User
 # 🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "NEXTRONBOT"
 ALIVE_PHOTTO = os.environ.get("ALIVE_PHOTTO" , None)
