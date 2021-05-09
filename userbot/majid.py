2f6a694af7c2ebcdc5b7a4344a29e0ff17a9edd3
majid-nex <77442341+majid-nex@users.noreply.github.com>
2021-05-10 00:57:03 +0530

    Create majid.py

---
+++ userbot/majid.py
@@ -0,0 +1,44 @@
+import asyncio
+from telethon import events
+from telethon.tl.types import ChannelParticipantsAdmins
+from userbot import ALIVE_NAME, StartTime
+from userbot.utils import admin_cmd
+from userbot import bot
+from telethon import version
+from math import ceil
+import json
+import random
+import re
+from telethon import events, errors, custom
+import io
+from userbot.helpers.functions import get_readable_time
+import time
+import os
+import datetime
+#importing finished
+from userbot import botnickname 
+BOT = str(botnickname) if botnickname else "𝚄𝚂𝙴𝚁𝙱𝙾𝚃"
+NAME = str(ALIVE_NAME) if ALIVE_NAME else "Nextron-bot"
+tim = get_readable_time((time.time() - StartTime))
+#pic 👇
+PIC = os.environ.get("ALIVE_PIC")
+#op 
+uptime = tim
+#time = date + time okay
+TIME = time.asctime(time.localtime())
+#my name 👇
+𝗔𝗥𝗖𝗔𝗡𝗘 = "[NEXTRON](https://t.me/ZerotwoSupport)"
+#my bots repo 👇
+REPO = "[𝗕𝗢𝗧](https://github.com/majid-nex/nextron-bot)"
+#grpup👇NAME = "[{MASTER}](tg://user?id={X})"
+#yrr isko apne bot me aply krne se pehle mere se pooch lena ok
+#aur aage add kruga abhi busy okay 🤔
+global ghanti
+X = bot.uid
+MASTER = f"[{NAME}](tg://user?id={X})"
+GROUP = "[SUPPORT GROUP](https://t.me/ZerotwoSupport)"
+#itna test h aur aage krte h
+#test successful raha ab aage 
+ALIVE = " 𝓊𝓈ℯ𝓇𝒷ℴ𝓉 𝒾𝓈 ℴ𝓃 𝒻𝒾𝓇ℯ 🔥" #made by majid
+OP = "𝙷𝙴𝙻𝙻𝙾 𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 Nextron 𝚄𝚂𝙴𝚁𝙱𝙾𝚃 𝙸 𝙰𝙼 𝚃𝙷𝙴 𝙱𝙴𝚂𝚃 𝚄𝚂𝙴𝚁𝙱𝙾𝚃💝"
+EMOJI = "🔥"
